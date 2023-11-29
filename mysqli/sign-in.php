d5d417f4789ba1a8199ac3370942e7469b32c9b1
Rizqi Cahya Angelita <90441810+rizqicahyaa@users.noreply.github.com>
2023-11-30 02:03:07 +0700

    Update sign-in.php

--- mysqli/sign-in.php
+++ mysqli/sign-in.php
@@ -31,9 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="AdminKit">
-	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
-
+	
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
 
