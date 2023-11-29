d97f1da77e49fa64ebdff99810da1fa522fa18be
Rizqi Cahya Angelita <90441810+rizqicahyaa@users.noreply.github.com>
2023-11-30 02:07:49 +0700

    Update datauser.php

--- pdo/datauser.php
+++ pdo/datauser.php
@@ -13,13 +13,8 @@ $condition = $search ? "WHERE name LIKE '%$search%' OR email LIKE '%$search%'" :
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="description" content="Responsive Admin &amp; Dashboard Template based on Bootstrap 5">
-	<meta name="author" content="AdminKit">
-	<meta name="keywords" content="adminkit, bootstrap, bootstrap 5, admin, dashboard, template, responsive, css, sass, html, theme, front-end, ui kit, web">
 
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link rel="shortcut icon" href="img/icons/icon-48x48.png" />
-
-	<link rel="canonical" href="https://demo-basic.adminkit.io/" />
 
 	<title>Tugas 5 PHP PDO</title>
 
